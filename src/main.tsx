fe26b5e434570035de5bd98cda036027af0d303b
Niels <n.koster@portavita.eu>
2023-01-01 22:48:03 +0100

    Use index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import {HomeProvider} from './context/HomeContext'
 import {MarkersProvider} from './context/MarkersContext'
-// import './index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
