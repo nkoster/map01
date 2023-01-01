ac0f3c2fc43017b4170e17ccbec5ae5122e446b3
Niels <n.koster@portavita.eu>
2023-01-01 22:42:08 +0100

    Better layout for UX

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,12 @@ import {HomeTypes} from './context/HomeReducers'
 import {HomeContext} from './context/HomeContext'
 import Timer from './components/Timer'
 import {MarkersContext} from './context/MarkersContext'
-import {getDistanceFromLatLonListInKm, getPolylineArea} from './util'
+import {getDistanceFromLatLonListInKm, getPolygonDistanceFromLatLonListInKm, getPolylineArea} from './util'
 import {MarkerTypes} from './context/MarkersReducer'
+
+const PRECISION = 5
+const FACTOR_MILES_DISTANCE = 0.621371
+const FACTOR_MILES_AREA = 2.589988110336
 
 function App() {
 
@@ -20,6 +24,7 @@ function App() {
   const {markers} = markersState
 
   const [polygon, setPolygon] = useState<any>(false)
+  const [satellite, setSatellite] = useState<any>(false)
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -52,44 +57,70 @@ function App() {
   }, [home])
 
   if (home.lat === 0 && home.lng === 0) {
-    return <div>Loading your approximate location... <Timer /></div>
+    return <div style={{fontFamily:'sans-serif', color:'white'}}>Loading your approximate location... <Timer /></div>
   }
 
   const distance = getDistanceFromLatLonListInKm(markers)
 
   const area = markers.length > 2 ? getPolylineArea(markers) : 0
 
+  const polygonDistance = getPolygonDistanceFromLatLonListInKm(markers)
+
   return (
     <div className="App">
       <header>
-        {markers.length} marker{markers.length != 1 ? 's' : ''} &nbsp; &nbsp;
-        total distance {distance.toFixed(4)} km /&nbsp;
-        {(distance * 0.621371).toFixed(4)} mi &nbsp; &nbsp;
-        area {area.toFixed(4)} km<sup>2</sup> /&nbsp;
-        {(area / 2.589988110336).toFixed(4)} mi<sup>2</sup> &nbsp; &nbsp; &nbsp;
-        <button
-          disabled={markers.length < 3}
-          onClick={() => setPolygon(!polygon)}
-        >{polygon ? 'markers' : 'polygon'}</button> &nbsp;
-        <button
-          disabled={markers.length == 0 || polygon}
-          onClick={() => confirm('You are about to remove ALL current markers from the map.\n\nAre you sure?')
-            && markersDispatch({type: MarkerTypes.Update, payload: []})}
-        >Reset</button>
+        <span className={'info'}>
+          {markers.length > 1 ? polygon ? 'polygon / ' : markers.length == 2 ? 'line / ' : 'polyline / ' : ''}
+          {markers.length} {polygon ? 'vertice' : 'point'}{markers.length != 1 ? 's' : ''}
+        </span> &nbsp; &nbsp;
+        {!polygon && <span className={'info'}>
+          distance {distance.toFixed(PRECISION)} km /&nbsp;
+          {(distance * FACTOR_MILES_DISTANCE).toFixed(PRECISION)} mi
+        </span>}
+        {polygon && <span className={'info'}>
+          perimeter {polygonDistance.toFixed(PRECISION)} km /&nbsp;
+          {(polygonDistance * FACTOR_MILES_DISTANCE).toFixed(PRECISION)} mi
+        </span>} &nbsp; &nbsp;
+        {polygon && <span className={'info'}>
+          area {area.toFixed(PRECISION)} km<span style={{display:'inline-block',width:'3px'}}></span>² /&nbsp;
+          {(area / FACTOR_MILES_AREA).toFixed(PRECISION)} mi<span style={{display:'inline-block',width:'3px'}}></span>²
+        </span>}
       </header>
-      <main style={{paddingTop:'2rem'}}>
+      <main style={{paddingTop:0}}>
         <MapContainer
-          style={{ height: 'calc(100vh - 4rem)', width: '100%', cursor: 'crosshair' }}
+          style={{ height: 'calc(100vh - 15px)', width: '100%', cursor: 'crosshair' }}
           center={home}
           zoom={16}
           scrollWheelZoom={true}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | w3b dot net'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            // url = 'https://api.mapbox.com/v4/mapbox.terrain-rgb/${z}/${x}/${y}.pngraw'
+            url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+            // url = 'https://server.arcgisonline.com/ArcGIS/rest/services/WorldElevation3D/Terrain3D/ImageServer/tile/{z}/{y}/{x}'
+            // attribution = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
           />
+          <TileLayer
+            opacity={satellite ? 0.5 : 1}
+            url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution = '&copy; OpenStreetMap | &copy Esri'
+          />
+          {/*</LayersControl>*/}
           <LocationMarkers polygon={polygon}/>
         </MapContainer>
+        <div className={'buttonBox'}>
+          <button
+            disabled={markers.length < 3}
+            onClick={() => setPolygon(!polygon)}
+          >{polygon ? 'polyline' : 'area'}</button> &nbsp;
+          <button
+            onClick={() => setSatellite(!satellite)}
+          >{satellite ? 'map' : 'satellite'}</button> &nbsp;
+          <button
+            disabled={markers.length == 0 || polygon}
+            onClick={() => confirm('You are about to remove ALL current markers from the map.\n\nAre you sure?')
+              && markersDispatch({type: MarkerTypes.Update, payload: []})}
+          >reset</button>
+        </div>
       </main>
     </div>
   )
